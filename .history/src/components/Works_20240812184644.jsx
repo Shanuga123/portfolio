a53d75ce4fc7ed8d388e78d55f694384068c58d1
Shanuga123 <mshanuga2000@gmail.com>
2024-08-12 18:48:17 +0530

    committed

--- .history/src/components/Works_20240812184644.jsx
+++ .history/src/components/Works_20240812184644.jsx
@@ -1,11 +1,12 @@
-import { motion } from "framer-motion";
 import React from "react";
-import { Tilt } from 'react-tilt';
+npm install react-parallax-tilt
 
-import { github } from "../assets";
-import { projects } from "../constants";
-import { SectionWrapper } from "../hoc";
+import { motion } from "framer-motion";
+
 import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
--- .history/src/components/Works_20240812184657.jsx
+++ .history/src/components/Works_20240812184657.jsx
@@ -1,11 +1,11 @@
-import { motion } from "framer-motion";
 import React from "react";
 import { Tilt } from 'react-tilt';
+import { motion } from "framer-motion";
 
-import { github } from "../assets";
-import { projects } from "../constants";
-import { SectionWrapper } from "../hoc";
 import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
