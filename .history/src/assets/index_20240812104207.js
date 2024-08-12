0560caad6f76554cf973377e4e521d8218cd3bee
Shanuga123 <mshanuga2000@gmail.com>
2024-08-12 13:33:53 +0530

    committed

---
+++ .history/src/assets/index_20240812104207.js
@@ -0,0 +1,44 @@
+import backend from "./backend.png";
+import close from "./close.svg";
+import creator from "./creator.png";
+import github from "./github.png";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
+import web from "./web.png";
+
+import a2z from "./company/a2z.png";
+import meta from "./company/meta.png";
+import XE from "./company/XE.png";
+import YIT from "./company/YIT.png";
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
+import wordpress from "./tech/wordpress.png";
+import ex from "./"
+
+import ajax from "./ajax.jpeg";
+import recipe from "./recipe.jpeg";
+import vegify from "./vegify.jpeg";
+
+export {
+  XE, YIT, a2z, backend, close, creator, css,
+  docker,
+  figma,
+  git, github, html,
+  javascript, ajax, logo, menu, meta, mobile, mongodb,
+  nodejs,
+  reactjs, recipe, redux,
+  tailwind, threejs, typescript, vegify, web, wordpress
+};
+
